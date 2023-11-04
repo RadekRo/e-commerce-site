896595f6948b56760c93fe6ca9d3bede9d1a0730
RadekRo <r.roclawski@ankara.pl>
2023-11-04 15:39:49 +0100

    Working

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import HelloWorld from './HelloWorld/HelloWorldArrow';
-import Greetings from './Greetings/Greetings';
-import GreetingsClass from './Greetings/GreetingsClass';
-import UsersList from './UserList/UserList';
-import Counter from './Counter/Counter';
-import LoginForm from './LoginForm/LoginForm';
-=======
 import Header from './Header/Header';
 import InventoryItem from './InventoryItem/InventoryItem';
 import Blank from './Blank/Blank';
-import CounterClass from './counter/CounterClass';
+import LoginForm from './LoginForm/LoginForm';
 
->>>>>>> 324a33bfaf466875c3022abb0b62f946ab6c1bed
 
 function App() {
   return (
       <div className="App">
-<<<<<<< HEAD
          {/* <HelloWorld />
           <Greetings name="Bartosz" />
           <GreetingsClass name="Dziubku" />
           <UsersList />
-          <Counter /> */}
-          <LoginForm /> 
-=======
+          <Counter />
           <Header title="hurtownia badziewia"/>
           <InventoryItem name="garbus" price="100"/>
           <InventoryItem name="konik" price="99"/>
           {/* <Blank /> */}
-          <CounterClass name="Greg"/>
->>>>>>> 324a33bfaf466875c3022abb0b62f946ab6c1bed
+          <LoginForm /> 
     </div>
   );
 }
