656793b9a27025307b2bce3e8219c99f18b5af3e
RadekRo <r.roclawski@ankara.pl>
2023-11-04 15:35:04 +0100

    Next stage

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -10,6 +10,7 @@ class LoginForm extends Component {
              email : "",
              errors : []
         }
+        this.validatePasswordOnBlur = this.validatePasswordOnBlur.bind(this)
     }
     validateNotEmpty(field, value)
     {
@@ -18,28 +19,97 @@ class LoginForm extends Component {
             return `${field} must be filled out`
         }
     }
+    checkErrorExist(errors, error) 
+    {
+        return errors.includes(error)
+    }
     validateUsernameOnBlur(event) 
     {
-        console.log(event)
-        console.log(this)
-        const username = event.target.value;
-        const errors = this.state.errors;
+        const username = event.target.value
+        const errors = this.state.errors
 
         var validate_result = this.validateNotEmpty("Username", username)
         if (validate_result) {
             this.setState({username : ""})
-            errors.push(validate_result);
+            if(!this.checkErrorExist(errors, validate_result))
+            {
+                errors.push(validate_result)
+            }
         }
 
         this.setState({username, errors})
-        console.log(this.state)
+        //console.log(this.state)
 
     }
+    
+    validatePasswordOnBlur(event) 
+    {
+        const password = event.target.value
+        const errors = this.state.errors
+
+        var validate_result = this.validateNotEmpty("Password", password)
+        if(validate_result)
+        {
+            this.setState({password : ""})
+            if(!this.checkErrorExist(errors, validate_result))
+            {
+                errors.push(validate_result)
+            }
+        }
+        this.setState({password, errors})
+    }
+
+    validatePasswordConfirmationOnBlur(event)
+    {
+        const passwordConfirmation = event.target.value
+        const errors = this.state.errors
+
+        if(passwordConfirmation !== this.state.password)
+        {
+            var error = "Password must match"
+            if(!this.checkErrorExist(errors, error))
+            {
+                errors.push(error)
+            }
+        }
+        this.setState({passwordConfirmation, errors})
+    }
+
+    validateEmailFormat(fieldName, value) 
+    {
+        let [lhs, rhs] = value.split('@')
+        lhs = lhs || ""
+        rhs = rhs || ""
+        
+        if(lhs.length <= 0 || rhs.length <= 0) 
+        {
+            return `${fieldName} must be in standard emailformat`
+        }
+    }
+
+    validateEmailOnBlur(event) 
+    {
+        const email = event.target.value
+        const errors = this.state.errors
+
+        var validate_result = this.validateEmailFormat("Email", email)
+        if(validate_result)
+        {
+            this.setState({email : ""})
+            errors.push(validate_result)
+        }
+    }
+    
+    submit() 
+    {
+        alert("Submitted!")
+    }
+
     displayErrors() 
     {
         return (
-            <div>
-                {this.state.errors.map((err, index) => <p>{err}</p>)}
+            <div id="errorsDiv">
+                {this.state.errors.map((err, index) => <p key={index}>{err}</p>)}
             </div>
         )
 
@@ -49,10 +119,10 @@ class LoginForm extends Component {
         return (
             <div className="App">
                 Username: <input type="text" onBlur={this.validateUsernameOnBlur.bind(this)} /><br />
-                Password: <input type="text" /><br />
-                Password Confirmation: <input type="text" /><br />
-                Email: <input type="text" /><br />
-                <button>Submit</button>
+                Password: <input type="text" onBlur={this.validatePasswordOnBlur} /><br />
+                Password Confirmation: <input type="text" onBlur={this.validatePasswordConfirmationOnBlur.bind(this)} /><br />
+                Email: <input type="text" onBlur={this.validateEmailOnBlur.bind(this)} /><br />
+                <button onClick={this.submit}>Submit</button>
                 {/*console.log(this.state.errors)*/}
                 {this.displayErrors()}
             </div>)
